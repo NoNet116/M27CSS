1c067ee038f439edc0fb0038c1dc92b0163dfdcc
Name <email@example.ru>
2025-03-04 18:02:21 +0300

    Добавлен фильтр в строке поиска

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -11,3 +11,24 @@ else {
     alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
     window.location.href = "http://www.google.com"
 }*/
+
+document.addEventListener("DOMContentLoaded", function () {
+    let inputElement = document.querySelector(".search-field");
+   // let buttonElement = document.querySelector(".search-button");
+
+    // Фильтрация при вводе
+    inputElement.addEventListener("input", filterVideos);
+
+    // Фильтрация при клике на кнопку
+   // buttonElement.addEventListener("click", filterVideos);
+
+    function filterVideos() {
+        let inputString = inputElement.value.toLowerCase();
+        let elements = document.getElementsByClassName("video-container");
+
+        for (let e of  elements) {
+            let videoText = e.querySelector(".video-title").innerText.toLowerCase();
+            e.style.display = videoText.includes(inputString) ? 'block' : 'none';
+        }
+    }
+});
